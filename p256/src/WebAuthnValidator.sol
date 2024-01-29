d386b49da014318292feec63340564ef00cec816
adnpark <aidenp.dev@gmail.com>
2024-01-29 22:13:44 +0900

    chore: update comments

--- p256/src/WebAuthnValidator.sol
+++ p256/src/WebAuthnValidator.sol
@@ -8,33 +8,54 @@ import {IKernelValidator} from "kernel/src/interfaces/IKernelValidator.sol";
 import {ValidationData} from "kernel/src/common/Types.sol";
 import {SIG_VALIDATION_FAILED} from "kernel/src/common/Constants.sol";
 
-// TODO: add comments
-
+// public key
 struct WebAuthnValidatorData {
     uint256 x;
     uint256 y;
 }
 
+/**
+ * @title WebAuthnValidator
+ * @notice This validator uses the P256 curve to validate signatures.
+ */
 contract WebAuthnValidator is IKernelValidator {
+    // Emitted when a bad key is provided.
     error InvalidPublicKey();
 
+    // Emitted when the public key of a kernel is changed.
     event WebAuthnPublicKeyChanged(address indexed kernel, WebAuthnValidatorData newPubKey);
 
+    // The P256 public keys of a kernel.
     mapping(address kernel => WebAuthnValidatorData WebAuthnValidatorData) public webAuthnValidatorStorage;
 
+    /**
+     * @notice Enable WebAuthn validator for a kernel account.
+     * @dev The kernel account need to be the `msg.sender`.
+     * @dev The public key is encoded as `abi.encode(WebAuthnValidatorData)` inside the data, so (uint256,uint256).
+     */
     function enable(bytes calldata _data) external payable override {
+        // check validity of the public key
         WebAuthnValidatorData memory pubKey = abi.decode(_data, (WebAuthnValidatorData));
         if (pubKey.x == 0 || pubKey.y == 0) {
             revert InvalidPublicKey();
         }
+        // Update the key (so a sstore)
         webAuthnValidatorStorage[msg.sender] = pubKey;
+        // And emit the event
         emit WebAuthnPublicKeyChanged(msg.sender, pubKey);
     }
 
+    /**
+     * @notice Disable WebAuthn validator for a kernel account.
+     * @dev The kernel account need to be the `msg.sender`.
+     */
     function disable(bytes calldata) external payable override {
         delete webAuthnValidatorStorage[msg.sender];
     }
 
+    /**
+     * @notice Validate a user operation.
+     */
     function validateUserOp(UserOperation calldata _userOp, bytes32 _userOpHash, uint256)
         external
         payable
@@ -44,6 +65,9 @@ contract WebAuthnValidator is IKernelValidator {
         return _verifySignature(_userOp.sender, _userOpHash, _userOp.signature);
     }
 
+    /**
+     * @notice Validate a signature.
+     */
     function validateSignature(bytes32 hash, bytes calldata signature)
         external
         view
@@ -53,11 +77,15 @@ contract WebAuthnValidator is IKernelValidator {
         return _verifySignature(msg.sender, hash, signature);
     }
 
+    /**
+     * @notice Verify a signature.
+     */
     function _verifySignature(address sender, bytes32 hash, bytes calldata signature)
         private
         view
         returns (ValidationData)
     {
+        // decode the signature
         (
             bytes memory authenticatorData,
             string memory clientDataJSON,
@@ -67,8 +95,10 @@ contract WebAuthnValidator is IKernelValidator {
             uint256 s
         ) = abi.decode(signature, (bytes, string, uint256, uint256, uint256, uint256));
 
+        // get the public key from storage
         WebAuthnValidatorData memory pubKey = webAuthnValidatorStorage[sender];
 
+        // verify the signature using the signature and the public key
         bool isValid = WebAuthn.verifySignature(
             abi.encodePacked(hash),
             authenticatorData,
@@ -82,6 +112,7 @@ contract WebAuthnValidator is IKernelValidator {
             pubKey.y
         );
 
+        // return the validation data
         if (isValid) {
             return ValidationData.wrap(0);
         }
